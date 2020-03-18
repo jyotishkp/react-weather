8e77cb06135787702dd7437fc6776f3650cba77e
Jyotish <jyotish.pillai@digitalauthority.me>
2020-03-17 23:55:55 -0500

    Responsive React Weather App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,136 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Moment from 'react-moment';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Table from 'react-bootstrap/Table';
+import Navbar from 'react-bootstrap/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './css/App.css';
+import './css/owfont-regular.min.css';
+import getWeather from './api';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      weather: {},
+      sunrise: '',
+      sunset: '',
+      main: '',
+      wind: ''
+    }
+  }
+
+  componentDidMount() {
+    let that = this;
+    navigator.geolocation.getCurrentPosition(getWeatherData);
+
+    function getWeatherData(position) {
+      getWeather(position).then(res => {
+
+        let dayNight = res.data.weather[0].icon.split('')[2] === 'd' ? 'day' : 'night';
+
+        that.setState({
+          weather: res.data.weather[0],
+          sunrise: res.data.sys.sunrise,
+          sunset: res.data.sys.sunset,
+          main: res.data.main,
+          wind: res.data.wind,
+          place: res.data.name,
+          dayNight: dayNight
+        });
+      });
+    }
+  }
+
+  render() {
+    let hasTemp = this.state.main.temp;
+    let temp;
+    let today = new Date();
+    if (!hasTemp) {
+      temp = <span>Loading....</span>;
+    } else {
+      temp = <span className="temp">{Math.round(this.state.main.temp)}&deg;C</span>;
+    }
+
+    return (
+      <div className="App">
+        <Navbar bg="dark" variant="dark" fixed="top">
+          <Navbar.Brand>
+            <i className='owf owf-201'></i>{' '}
+          Weather
+            </Navbar.Brand>
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              {this.state.place}
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Navbar>
+        <Container fluid className={this.state.dayNight}>
+          <Row>
+            <Col sm={12} md={6}>
+              <Row >
+                <Col>
+                  {temp}
+                </Col>
+              </Row>
+              <Row >
+                <Col className="weather-icon">
+                  <i className={`owf owf-${this.state.weather.id}`}></i>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <span className="description">{this.state.weather.description} . </span><Moment format="MMMM Do YYYY . dddd">{today}</Moment>
+                </Col>
+              </Row>
+            </Col>
+            <Col sm={12} md={6} className="detail-col">
+              <Table className="weather-detail" variant="dark">
+                <tbody>
+                  <tr>
+                    <td>Feels Like</td>
+                    <td>{this.state.main.feels_like}&deg;C</td>
+                  </tr>
+                  <tr>
+                    <td>Max</td>
+                    <td>{this.state.main.temp_max} &deg;C</td>
+                  </tr>
+                  <tr>
+                    <td>Min</td>
+                    <td>{this.state.main.temp_min} &deg;C</td>
+                  </tr>
+                  <tr>
+                    <td>Pressure</td>
+                    <td>{this.state.main.pressure} hPa</td>
+                  </tr>
+                  <tr>
+                    <td>Humidity</td>
+                    <td>{this.state.main.humidity}%</td>
+                  </tr>
+                  <tr>
+                    <td>Wind Speed</td>
+                    <td>{this.state.wind.speed}m/s</td>
+                  </tr>
+                  <tr>
+                    <td>Sunrise</td>
+                    <td><Moment unix format="h:mm a">{this.state.sunrise}</Moment></td>
+                  </tr>
+                  <tr>
+                    <td>Sunset</td>
+                    <td><Moment unix format="h:mm a">{this.state.sunset}</Moment></td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/api.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+function getWeather(position) {
+    return axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&APPID=0ba9b446415312d26e56f2196b060cd5&units=Metric`);
+}
+
+export default getWeather;
